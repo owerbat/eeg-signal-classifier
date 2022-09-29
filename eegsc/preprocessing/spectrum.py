7356c4e948654cc640a4414a83fd51245d799aac
owerbat <niktimak@gmail.com>
2022-09-30 00:10:33 +0400

    Frequency statistics

--- eegsc/preprocessing/spectrum.py
+++ eegsc/preprocessing/spectrum.py
@@ -1,13 +1,15 @@
 import numpy as np
 
 from eegsc.preprocessing.filters import BandPassFilter
-from eegsc.preprocessing.statistics import AmplitudeStatisticsTransformer
+from eegsc.preprocessing.statistics import AmplitudeStatisticsTransformer, \
+    FrequencyStatisticsTransformer
 
 
 class SpectrumTransformer:
     def __init__(self,
                  order: int = 2,
-                 signal_duration: float = 10) -> None:
+                 signal_duration: float = 10,
+                 psd_method: str = 'periodogram') -> None:
         self.filters = {
             'delta': BandPassFilter(order, 1, 4, signal_duration),
             'theta': BandPassFilter(order, 4, 8, signal_duration),
@@ -16,21 +18,28 @@ class SpectrumTransformer:
             'gamma': BandPassFilter(order, 20, 50, signal_duration),
         }
         self.ampl_transformer = AmplitudeStatisticsTransformer()
+        self.freq_transformer = FrequencyStatisticsTransformer(
+            psd_method=psd_method,
+            signal_duration=signal_duration
+        )
+
+        self.transformers = [self.ampl_transformer, self.freq_transformer]
 
         self.columns = []
-        for name in ['original'] + list(self.filters.keys()):
-            self.columns += [f'{name}_{col}' for col in self.ampl_transformer.columns]
+        for transformer in self.transformers:
+            for name in ['original'] + list(self.filters.keys()):
+                self.columns += [f'{name}_{col}' for col in transformer.columns]
 
     def transform(self, signals: np.ndarray):
         spectrum_signals = {'original': signals}
         for name, filter in self.filters.items():
             spectrum_signals[name] = filter.filter(signals)
 
-        spectrum_ampl_statistics = [
-            self.ampl_transformer.transform(spectrum_signal)
-            for _, spectrum_signal in spectrum_signals.items()
-        ]
+        spectrum_statistics = []
+        for transformer in self.transformers:
+            spectrum_statistics += [
+                transformer.transform(spectrum_signal)
+                for _, spectrum_signal in spectrum_signals.items()
+            ]
 
-        spectrum_freq_statistics = []
-
-        return np.hstack(spectrum_ampl_statistics + spectrum_freq_statistics)
+        return np.hstack(spectrum_statistics)
--- eegsc/preprocessing/statistics.py
+++ eegsc/preprocessing/statistics.py
@@ -1,10 +1,24 @@
+from math import ceil
 import numpy as np
+from scipy.signal import periodogram, welch
 
 
-class AmplitudeStatisticsTransformer:
-    def __init__(self) -> None:
+class BaseStatisticsTransformer:
+    def __init__(self, metrics: dict) -> None:
         self.sensors_num = 32
-        self.metrics = {
+        self.metrics = metrics
+
+        self.columns = []
+        for name in self.metrics.keys():
+            self.columns += [f'{name}_{i}' for i in range(self.sensors_num)]
+
+    def transform(self, signals: np.ndarray):
+        raise NotImplementedError()
+
+
+class AmplitudeStatisticsTransformer(BaseStatisticsTransformer):
+    def __init__(self) -> None:
+        metrics = {
             'mean': lambda x: np.nanmean(x, axis=-1),
             'std': lambda x: np.nanstd(x, axis=-1),
             'median': lambda x: np.nanmedian(x, axis=-1),
@@ -12,9 +26,48 @@ class AmplitudeStatisticsTransformer:
             'max': lambda x: np.nanmax(x, axis=-1),
         }
 
-        self.columns = []
-        for name in self.metrics.keys():
-            self.columns += [f'{name}_{i}' for i in range(self.sensors_num)]
+        super().__init__(metrics)
 
     def transform(self, signals: np.ndarray):
         return np.hstack([metric(signals) for _, metric in self.metrics.items()])
+
+
+class FrequencyStatisticsTransformer(BaseStatisticsTransformer):
+    def __init__(self,
+                 psd_method: str = 'periodogram',
+                 signal_duration: float = 10) -> None:
+        if psd_method not in ('periodogram', 'welch'):
+            raise ValueError('psd_method has to be one of the following: '
+                             '`periodogram`, `welch`')
+
+        self.psd_func = None
+        if psd_method == 'periodogram':
+            self.psd_func = periodogram
+        elif psd_method == 'welch':
+            self.psd_func = welch
+
+        self.signal_duration = signal_duration
+
+        metrics = {
+            'psd_mean': lambda x: np.nanmean(x, axis=-1),
+            'psd_std': lambda x: np.nanstd(x, axis=-1),
+            'psd_median': lambda x: np.nanmedian(x, axis=-1),
+            'psd_min': lambda x: np.nanmin(x, axis=-1),
+            'psd_max': lambda x: np.nanmax(x, axis=-1),
+        }
+
+        super().__init__(metrics)
+
+    def transform(self, signals: np.ndarray):
+        psd = np.full((signals.shape[0], signals.shape[1], ceil(signals.shape[2] / 2)),
+                      np.nan)
+
+        for i, signal in enumerate(signals):
+            actual_len = signal[0][~np.isnan(signal[0])].shape[0]
+            actual_signal = signal[:, :actual_len]
+            _, local_psd = self.psd_func(x=actual_signal,
+                                         fs=actual_len / self.signal_duration,
+                                         scaling='spectrum')
+            psd[i, :, :local_psd.shape[1]] = local_psd
+
+        return np.hstack([metric(psd) for _, metric in self.metrics.items()])
